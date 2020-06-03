610379a1ef1af0ae5dc11fa14f22c3659b74869a
Jorge Verdugo <jorgeverdugoch@gmail.com>
2020-06-03 19:55:38 -0400

    Se agrega la lógica del complemento

--- servicio/servicios.py
+++ servicio/servicios.py
@@ -2,8 +2,8 @@ from flask import jsonify
 
 
 def parsear_automata(automata):
-    E = automata['E']  # (Σ) Alfabeto
     K = automata['K']  # (K) Nodos
+    E = automata['E']  # (Σ) Alfabeto
     S = automata['S']  # (S) Iniciales
     F = automata['F']  # (F) Finales
     s = []             # (δ) Conexiones
@@ -11,11 +11,14 @@ def parsear_automata(automata):
     for conexion in automata['s']:
         tupla = (conexion['inicio'], arista['letra'], arista['final'])
         s.append(tupla)
-    return AutomataFinito(K, E, S, F, conexiones)
+    return AutomataFinito(E, K, S, F, s)
 
 
 def operar(json):
-    automata = parsear_automata(json['automatas'])
+    automata1 = parsear_automata(json['automatas'][0])
+    automata2 = parsear_automata(json['automatas'][1])
+    operacion = json['operacion']
+
     return jsonify(["uwu"])
 
 
