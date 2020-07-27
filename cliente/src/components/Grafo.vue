57fc8ad4ea310a34e4d524b0123cdbec8725e14e
Jorge Verdugo <jorgeverdugoch@gmail.com>
2020-07-27 00:20:07 -0400

    Se cambian íconos de nodos

--- cliente/src/components/Grafo.vue
+++ cliente/src/components/Grafo.vue
@@ -56,27 +56,42 @@ export default {
             },
           },
           {
-            selector: ".inicial",
+            selector: ".inicial.final",
             css: {
-              shape: "triangle",
+              shape: "square",
+              "background-image": require('../assets/nodo-if.png'),
+              "background-fit": "contain contain",
+              "background-color": "#fff",
             },
           },
           {
-            selector: ".noInicial",
-            css: {},
-          },
-          {
-            selector: ".final",
+            selector: ".inicial.noFinal",
             css: {
-              "background-color": "#58d5b2",
+              shape: "square",
+              "background-image": require('../assets/nodo-i.png'),
+              "background-fit": "contain cover",
+              "background-color": "#fff",
             },
           },
           {
-            selector: ".noFinal",
+            selector: ".noInicial.final",
             css: {
-              "background-color": "#c458d5",
+              shape: "square",
+              "background-image": require('../assets/nodo-f.png'),
+              "background-fit": "cover cover",
+              "background-color": "#fff",
             },
           },
+          {
+            selector: ".noInicial.noFinal",
+            css: {
+              shape: "square",
+              "background-image": require('../assets/nodo.png'),
+              "background-fit": "cover cover",
+              "background-color": "#fff",
+            },
+          },
+          
           {
             selector: "edge",
             css: {
@@ -165,6 +180,19 @@ export default {
       cy.layout(this.config.layout).run();
       // cy.style(this.config.style).update();
     },
+    buildNodo(esFinal, esInicial, colorPrincipal, colorFlecha) {
+      let data = `
+        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024.5 859.06">
+          <defs>
+            <style>.cls-1,.cls-2{fill:${colorPrincipal};}.cls-1,.cls-3{}.cls-3{fill:${colorFlecha ? colorFlecha : colorPrincipal};}</style>
+            </defs>
+          <circle class="cls-1" cx="594.97" cy="429.53" r="321.63"/>
+          ${esFinal ? '<path class="cls-2" d="M594.47,941.53a429.63,429.63,0,0,1-167.2-825.29,429.63,429.63,0,0,1,334.4,791.52A426.79,426.79,0,0,1,594.47,941.53Zm0-799.06A369.52,369.52,0,1,0,855.77,250.7,367.14,367.14,0,0,0,594.47,142.47Z" transform="translate(0.5 -82.47)"/>' : ''}
+          ${esInicial ? '<polygon class="cls-3" points="273.34 429.53 0.5 196.31 0.5 662.75 273.34 429.53"/>' : ''}
+        </svg>
+      `;
+      return 'data:image/svg+xml;base64,' + btoa(data);
+    }
   },
 };
 </script>
--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Presentacion from "../views/Presentacion";
 import AFD from "../views/AFD";
+import Simplificar from "../views/Simplificar";
 import Operar from "../views/Operar";
 
 Vue.use(VueRouter);
@@ -12,14 +13,19 @@ const routes = [{
         component: Presentacion
     },
     {
+        path: "/operar",
+        name: "operar",
+        component: Operar
+    },
+    {
         path: "/afd",
         name: "afd",
         component: AFD
     },
     {
-        path: "/operar",
-        name: "operar",
-        component: Operar
+        path: "/simplificar",
+        name: "simplificar",
+        component: Simplificar
     },
 ];
 
